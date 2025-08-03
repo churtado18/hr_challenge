78cdd3938c1bae1fe3df1c652a0ea8c89c26a938
Carmelo Hurtado Orozco <churtado@asesoftware.com>
2025-08-03 18:33:59 -0500

    Add limit to insert batch transactons

--- app/api/human_resources/dtos/job_dto.py
+++ app/api/human_resources/dtos/job_dto.py
@@ -1,9 +1,13 @@
-from typing import List
 from sqlmodel import SQLModel, Field
+from pydantic import BaseModel, conlist
+from typing import Annotated
 
 class JobCreate(SQLModel):
     id: int
     job: str = Field(max_length=300)
 
-class JobBatch(SQLModel):
-    jobs: List[JobCreate]
+class JobBatch(BaseModel):
+    jobs: Annotated[
+        conlist(item_type=JobCreate, min_length=1, max_length=1000),
+        ...
+    ]
